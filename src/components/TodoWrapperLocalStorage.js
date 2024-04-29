457ee0054ed716bff1da5aa213338e401e25315d
Kushal Shah <code@kushalshah.com.np>
2024-04-29 23:13:34 +0545

    addedLocalStorage

--- src/components/TodoWrapperLocalStorage.js
+++ src/components/TodoWrapperLocalStorage.js
@@ -3,10 +3,10 @@ import { TodoForm } from './TodoForm'
 import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
-uuidv4();
+uuidv4();   
 
 export const TodoWrapperLocalStorage = () => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]); 
 
     useEffect(() => {
         const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
